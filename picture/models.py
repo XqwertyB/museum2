4f6d86700b4b1d17651660ddd9a3f7576be41279
Behruz <87355568+XqwertyB@users.noreply.github.com>
2022-03-23 17:14:34 +0500

    as

--- picture/models.py
+++ picture/models.py
@@ -1,13 +1,14 @@
+from distutils.command.upload import upload
 from django.db import models
 
 
 class Picture_type(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null=False)
     def __str__(self):
         return self.name
 
 class Pictures(models.Model):
-    image = models.ImageField()
+    image = models.ImageField("Img", upload_to="Images/")
     name = models.CharField("Nomi", max_length=30)
     type = models.ForeignKey(Picture_type, on_delete=models.CASCADE,)
 
